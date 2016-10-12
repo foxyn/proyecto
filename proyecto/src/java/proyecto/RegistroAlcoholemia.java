06fec2e01fb338525825fbeaa8b4798ae932da81
felipe <felipe@Cloti-PC>
2016-10-12 19:18:40 -0300

    Se agrega entity RegistroAlcoholemia.

---
+++ proyecto/src/java/proyecto/RegistroAlcoholemia.java
@@ -0,0 +1,67 @@
+package proyecto;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author felipe
+ */
+@Entity
+@Table (name = "REGISTRO_ALCOHOLEMIA")
+public class RegistroAlcoholemia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_REGISTRO")
+    private Long id;
+    
+    @Temporal (TemporalType.TIMESTAMP)
+    @Column(name = "FECHA_CREACION")
+    private Date fechaCreacion;
+    
+   
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof RegistroAlcoholemia)) {
+            return false;
+        }
+        RegistroAlcoholemia other = (RegistroAlcoholemia) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "proyecto.RegistroAlcoholemia[ id=" + id + " ]";
+    }
+    
+}
