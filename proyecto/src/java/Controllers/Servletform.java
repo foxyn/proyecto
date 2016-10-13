bcf7c91132b5af9867b752390d64ffb2eaf86d0d
victorzepeda <victorzepeda@192.168.1.160>
2016-10-12 23:06:39 -0300

    Se agrega Servletform para realizar operaciones

--- proyecto/src/java/Controllers/Servletform.java
+++ proyecto/src/java/Controllers/Servletform.java
@@ -1,10 +1,8 @@
 
-package proyecto;
+package Controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,34 +10,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name = "Servlet", urlPatterns = {"/formulario_principal.html"})
-public class Servlet extends HttpServlet {
+@WebServlet(name = "Servletform", urlPatterns = {"/Servletform"})
+public class Servletform extends HttpServlet {
 
-    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-       
-    }
-
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
-        request.getRequestDispatcher("/form.jsp")
-                .forward(request, response);
+        
     }
 
-  
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-     
+      String correo = request.getParameter("correo");
+        response.setContentType("text/html;charset=UTF-8");
+        
+        try(PrintWriter out = response.getWriter()){
+            out.println("correo: "+ correo);
+        }
     }
 
-  
 
 
 }
